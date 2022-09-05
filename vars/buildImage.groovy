cc4383377fca7f1b7b610d0567f381b3c7466c5b
Mohamed Ahmed <58955899+mohamedAhmed97@users.noreply.github.com>
2022-09-05 14:58:30 +0200

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,8 @@
 def call(){
     echo "=========== bulding image ============"
         withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {  
-                        sh "docker build -t mar97/first-repositary:1.2 ."
+                        sh "docker build -t mar97/first-repositary:1.3 ."
                         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-                        sh "docker push mar97/first-repositary:1.2"
+                        sh "docker push mar97/first-repositary:1.3"
         }
 }
